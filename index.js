2f2b8ae608b5f287c59b53cf3c58d8248d9a1870
JhihYang Wu <jhihyang_wu@outlook.com>
2024-02-18 01:10:05 -0700

    remove armCPUonFPGA

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ let numAZAImages = 10
 let numBookTankImages = 6
 let numMinigradImages = 4
 let numControllerImages = 11
-let numArmImages = 4
 let numKewlAIImages = 3
 
 let cryptoPictureIndex = 1
@@ -49,7 +48,6 @@ let azaPictureIndex = 1
 let booktankPictureIndex = 1
 let minigradPictureIndex = 1
 let controllerPictureIndex = 1
-let armPictureIndex = 1
 let kewlaiPictureIndex = 1
 
 $("crypto-picture").src = "images/crypto/1.png"
@@ -57,7 +55,6 @@ $("aza-picture").src = "images/aza/1.png"
 $("booktank-picture").src = "images/booktank/1.png"
 $("minigrad-picture").src = "images/minigrad/1.png"
 $("controller-picture").src = "images/controller/1.png"
-$("arm-picture").src = "images/arm/1.png"
 $("kewlai-picture").src = "images/kewlai/1.png"
 
 function switchImage(project, right) {
@@ -121,18 +118,6 @@ function switchImage(project, right) {
             controllerPictureIndex = numControllerImages
         }
         $("controller-picture").src = `images/controller/${controllerPictureIndex}.png`
-    } else if (project == "arm") {
-        if (right) {
-            armPictureIndex++
-        } else {
-            armPictureIndex--
-        }
-        if (armPictureIndex > numArmImages) {
-            armPictureIndex = 1
-        } else if (armPictureIndex < 1) {
-            armPictureIndex = numArmImages
-        }
-        $("arm-picture").src = `images/arm/${armPictureIndex}.png`
     } else if (project == "kewlai") {
         if (right) {
             kewlaiPictureIndex++
